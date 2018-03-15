78a04f6fb4fd36a4a55e949b8e735bb98122e417
luferChan <chyou@vip.qq.com>
2018-03-15 13:05:52 +0800

    CookieUtil Test

---
+++ mmall/src/main/java/com/mmall/util/CookieUtil.java
@@ -0,0 +1,58 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private static String COOKIE_DOMAIN = ".lufernew.com";
+    private static String COOKIE_NAME = "mmall_login_token";
+
+    public static String readLoginToken(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null){
+            for(Cookie cookie : cookies){
+                log.info("Read CookieName:{},CookieValue:{}",cookie.getName(),cookie.getValue());
+                if (StringUtils.equals(cookie.getName(),COOKIE_NAME)){
+                    log.info("Return CookieName:{},CookieValue:{}",cookie.getName(),cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static void writeLoginToken(HttpServletResponse response,String token){
+        Cookie cookie = new Cookie(COOKIE_NAME,token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        cookie.setPath("/");
+
+        // 单位是秒，如不设置MaxAge，则Cookie不会存入硬盘，只会存入内存，即只在当前页有效。
+        // 设置-1 表示有效期为永久
+        cookie.setMaxAge(60*60*24*365);
+        log.info("Write CookieName:{},CookieValue:{}",cookie.getName(),cookie.getValue());
+        response.addCookie(cookie);
+    }
+
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        if (cookies!=null){
+            for(Cookie cookie : cookies){
+                if (StringUtils.equals(cookie.getName(),COOKIE_NAME)){
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);  //设置为0 等于删除此cookie
+                    log.info("Del CookieName:{},CookieValue:{}",cookie.getName(),cookie.getValue());
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
+    }
+}
